998b058ca317323ea464e5057623ad7ec36abcbd
Leichim <michielsite@hotmail.com>
2022-08-02 20:45:14 +0200

    Updated scoring

--- src/score.php
+++ src/score.php
@@ -12,9 +12,9 @@ defined( 'ABSPATH' ) or die('Nope...');
  */
 function miw_pb_score($number, $insert, $query, $delete): float {
 
-  $insert_score = max(1, 10 - ($insert/$number)/0.0025); // 0.0025s per post is a very good score for inserting
-  $query_score = max(1, 10 - ($query/$number)/0.000025); // 0.000025s per post is a very good score for querying
-  $delete_score = max(1, 10 - ($delete/$number)/0.00275); // 0.00275s per post is a very good score for deleting
+  $insert_score = max(1, 10 - ($insert/$number)/0.0015); // 0.0025s per post is a very good score for inserting
+  $query_score = max(1, 10 - ($query/$number)/0.000015); // 0.000025s per post is a very good score for querying
+  $delete_score = max(1, 10 - ($delete/$number)/0.0020); // 0.00275s per post is a very good score for deleting
 
-  return number_format( ($insert_score * 3 + $query_score + $delete_score * 3)/7, 2 );
+  return number_format( ($insert_score * 2 + $query_score + $delete_score * 2)/5, 2 );
 }
